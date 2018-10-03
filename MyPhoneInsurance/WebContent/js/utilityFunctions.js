beb2ed6258629f1965d4e4ae6ac085632b370453
Alex Doan <doan.95@buckeyemail.osu.edu>
2018-10-03 18:08:32 -0400

    Add JS web-content folder

---
+++ MyPhoneInsurance/WebContent/js/utilityFunctions.js
@@ -0,0 +1,21 @@
+function isValidQuantity(x) {
+  return !isNaN(x) && 
+     parseInt(Number(x)) == x && 
+     !isNaN(parseInt(x, 10)) &&
+     Number(x) > 0;
+}
+function validateQuantity() {
+	var items = document.forms["orderForm"].elements;
+	var formValid = true;
+	for (var i = 0; i < items.length - 2; i+=3) {
+		var quantity = items[i].value;
+		if(quantity.trim().length > 0 && !isValidQuantity(quantity)) { 
+			console.log("Invalid quantity: " + quantity)
+			formValid = false; 
+		}
+	}
+	if (!formValid) {
+		document.getElementById("invalid").innerHTML = "<p style=\'color: red;\'>Improper quantity for insurance added. Please use positive whole numbers.</p>"
+	}
+	return formValid;
+}
